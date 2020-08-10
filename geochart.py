d21549f834bd9a26c3aa14a8b4591580fd57468b
christie <christiecarolbeauchamp@gmail.com>
2020-08-10 19:47:50 +0100

    Got it to work using county names.

--- geochart.py
+++ geochart.py
@@ -16,14 +16,14 @@ if __name__=="__main__":
     counties = json.load(open(filename))
 
     df = pd.DataFrame({
-        'NUTS': ['UKC11', 'UK0'],
+        'NUTS': ['"Bristol, City of"', "Somerset"],
         'name': ['Jolene', 'Dolly']
         })
 
     import plotly.express as px
 
     fig = px.choropleth(df, geojson=counties, locations='NUTS', color='name',
-        featureidkey='properties.nuts318cd',
+        featureidkey='properties.nuts318nm',
         labels={'name':'name'}
         )
     fig.update_geos(fitbounds="locations", visible=False)
