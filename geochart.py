07633801b72d9d67b6db54f829e028ac3b26074f
christie <christiecarolbeauchamp@gmail.com>
2020-08-10 19:17:45 +0100

    Added data into geochart. It is not yet fully working.

--- geochart.py
+++ geochart.py
@@ -1,5 +1,7 @@
 import requests
 import json
+import pandas as pd
+
 
 if __name__=="__main__":
     json_url = 'https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json'
@@ -9,4 +11,18 @@ if __name__=="__main__":
 
     filename = 'geojson.json'
     counties = [json.loads(line) for line in open(filename)]
-    counties[0]['features']
+    
+    df = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/fips-unemp-16.csv",
+        dtype={"fips": str})
+
+    import plotly.express as px
+
+    fig = px.choropleth(df, geojson=counties, locations='fips', color='unemp',
+        color_continuous_scale="Viridis",
+        range_color=(0, 12),
+        scope="usa",
+        labels={'unemp':'unemployment rate'}
+        )
+    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+    fig.show()
+
