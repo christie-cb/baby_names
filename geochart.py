422768a079dd2db3d0239b860f76ff59c3907df4
christie <christiecarolbeauchamp@gmail.com>
2020-08-11 09:40:01 +0100

    Changed a few indices so that geochart would generate the chart. Doesn't yet display borders properly.:

--- geochart.py
+++ geochart.py
@@ -3,7 +3,8 @@ import json
 import pandas as pd
 
 def get_ons_nuts(ons_data, converter):
-    merged = ons_data.merge(converter, on="LAU118CD")
+    merged = ons_data.merge(converter, on="LAU118CD", how="left")
+    merged = merged.drop_duplicates('NUTS318CD')
     return merged
 
 def get_geojson_areanames(json_data):
@@ -17,16 +18,15 @@ if __name__=="__main__":
     filename = 'NUTS_Level_3__January_2018__Boundaries.geojson'
     counties = json.load(open(filename))
 
-    df = pd.DataFrame({
-        'NUTS': ['"Bristol, City of"', "Somerset"],
-        'name': ['Jolene', 'Dolly']
-        })
+    converter = pd.read_csv('areacd_converter.csv')
+    ons_data = pd.read_csv('top_by_region.csv')
+    ons_data = get_ons_nuts(ons_data, converter)
 
     import plotly.express as px
 
-    fig = px.choropleth(df, geojson=counties, locations='NUTS', color='name',
+    fig = px.choropleth(ons_data, geojson=counties, locations='NUTS318CD', color='girl_count',
         featureidkey='properties.nuts318cd',
-        labels={'name':'name'}
+        labels={'girl_count':'name'}
         )
     fig.update_geos(fitbounds="locations", visible=False)
     fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
