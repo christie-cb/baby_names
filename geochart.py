3f9c4bb832ef6cfdbb71bce676e17c6f02a8f5e1
christie <christiecarolbeauchamp@gmail.com>
2020-08-11 08:47:31 +0100

    Tidied up source of geojson data.

--- geochart.py
+++ geochart.py
@@ -4,15 +4,7 @@ import pandas as pd
 
 
 if __name__=="__main__":
-    json_url = 'https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json'
-    uk_json_url = 'https://raw.githubusercontent.com/deldersveld/topojson/master/countries/united-kingdom/uk-counties.json' 
-    response = requests.get(json_url)
-    with open('geojson.json', mode='wb') as json_file:
-        json_file.write(response.content)
-
-    #filename = 'geojson.json'
-    #counties = [json.loads(line) for line in open(filename)]
-    filename = '/Users/ccb/Desktop/NUTS_Level_3__January_2018__Boundaries.geojson'
+    filename = 'NUTS_Level_3__January_2018__Boundaries.geojson'
     counties = json.load(open(filename))
 
     df = pd.DataFrame({
